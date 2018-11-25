2ccdd739892efc2a72f3d22a7dc32b7e2fdf60ac
Jérôme BAR <jbar@norsys.fr>
2018-11-25 21:04:34 +0100

    move to next or prev slide with buttons

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
 <carousel>
-
+  <carousel-slide>
+    <div style="position:absolute; left:0; right:0; text-align:center; top:50%; color:#fff; font-size:16px,">Salut les gens</div>
+    <img src="http://lorempicsum.com/futurama/630/300/1" width=100%>
+  </carousel-slide>
+  <carousel-slide>
+    <div style="position:absolute; left:0; right:0; text-align:center; top:50%; color:#fff; font-size:16px,">Au revoir les gens</div>
+    <img src="http://lorempicsum.com/futurama/630/300/2" width=100%>
+  </carousel-slide>
 </carousel>
 </template>
 
--- src/components/carousel/Carousel.vue
+++ src/components/carousel/Carousel.vue
@@ -1,6 +1,8 @@
 <template>
-    <div>
+    <div class="carousel">
         <slot></slot>
+        <button class="carousel__nav carousel__prev" @click.prevent="prev"></button>
+        <button class="carousel__nav carousel__next" @click.prevent="next"></button>
     </div>
 </template>
 
@@ -8,8 +10,58 @@
 export default {
     data () {
         return {
-            
+            index: 0,
+            slides: [],
+            direction: null
+        }
+    },
+    mounted () {
+        this.slides = this.$children
+        this.slides.forEach((slide, i) => {
+            slide.index = i
+        })
+    },
+    computed: {
+        slidesCount () {
+            return this.slides.length
+        }
+    },
+    methods: {
+        next () {
+            this.index++
+            this.direction = 'right'
+            if (this.index >= this.slidesCount) {
+                this.index = 0
+            }
+        },
+        prev () {
+            this.index--
+            this.direction = 'left'
+            if (this.index < 0) {
+                this.index = this.slidesCount - 1
+            }
         }
     }
 }
 </script>
+<style>
+    .carousel {
+        position: relative;
+    }
+    .carousel__nav {
+        position: absolute;
+        top: 50%;
+        margin-top: -30px;
+        left: 10px;
+        background-image: url(prev.png);
+        width: 60px;
+        height: 60px;
+        border: none;
+    }
+    .carousel__nav.carousel__next {
+        right: 10px;
+        left: auto;
+        background-image: url(next.png)
+    }
+
+</style>
