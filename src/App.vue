0a4b550eeb34a26210495f8b114c6867819b3ad2
jbar <jerome.bar@prestataires.boulanger.com>
2018-11-27 13:20:02 +0100

    add or revome slides

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <template>
-<carousel>
-  <carousel-slide>
-    <div style="position:absolute; left:0; right:0; text-align:center; top:50%; color:#fff; font-size:16px,">Salut les gens</div>
-    <img src="http://lorempicsum.com/futurama/630/300/1" width=100%>
-  </carousel-slide>
-  <carousel-slide>
-    <div style="position:absolute; left:0; right:0; text-align:center; top:50%; color:#fff; font-size:16px,">Au revoir les gens</div>
-    <img src="http://lorempicsum.com/futurama/630/300/2" width=100%>
-  </carousel-slide>
-</carousel>
+  <div>
+    <carousel>
+      <carousel-slide v-for="n in slides" :index="n - 1">
+        <div style="position:absolute; left:0; right:0; text-align:center; top:50%; color:#fff; font-size:16px,">Slides
+          {{n}}
+        </div>
+        <img :src="`http://lorempicsum.com/futurama/630/300/${n}`" width=100%>
+      </carousel-slide>
+    </carousel>
+    <button @click="addSlide">Ajouter un slide</button>
+    <button @click="removeSlide">Supprimer un slide</button>
+  </div>
 </template>
 
 <script>
 
-import Carousel from './components/carousel/Carousel'
-import CarouselSlide from './components/carousel/CarouselSlide'
-export default {
-  name: 'app',
-  components: {
-    Carousel,
-    CarouselSlide
+  import Carousel from './components/carousel/Carousel'
+  import CarouselSlide from './components/carousel/CarouselSlide'
+
+  export default {
+    data() {
+      return {
+        name: 'app',
+        slides: 5
+      }
+    },
+    methods: {
+      addSlide() {
+        this.slides++
+      },
+      removeSlide() {
+        this.slides--
+      }
+    },
+    components: {
+      Carousel,
+      CarouselSlide
+    }
   }
-}
 </script>
--- src/components/carousel/CarouselSlide.vue
+++ src/components/carousel/CarouselSlide.vue
@@ -1,77 +1,93 @@
 <template>
   <transition :name="transition">
-        <div v-show="visible">
-        Index: {{ index }}
-        visible ? {{ visible }}
-        <slot></slot>
+    <div v-show="visible">
+
+      <slot></slot>
     </div>
   </transition>
 </template>
 
 <script>
-export default {
-    data () {
-        return {
-            index: 0
-        }
+  export default {
+    props: {
+      index: Number,
+      default: 0
     },
     computed: {
-        transition () {
-            if (this.$parent.direction) {
-                return `slide-${this.$parent.direction}`
-            }
-        },
-        visible () {
-            return this.index === this.$parent.index
+      transition() {
+        if (this.$parent.direction) {
+          return `slide-${this.$parent.direction}`
         }
+      },
+      visible() {
+        return this.index === this.$parent.index
+      }
     }
-}
+  }
 </script>
 
 <style>
-    .slide-right-enter-active {
-        animation: slideRightIn 5s
-    }
+  .slide-right-enter-active {
+    animation: slideRightIn .5s
+  }
 
-    .slide-right-leave-active {
-        animation: slideRightOut 5s;
-        position: absolute;
-        top: 0;
-        left: 0;
-        right: 0;
-        bottom: 0;
-        width: 100%;
-    }
+  .slide-right-leave-active {
+    animation: slideRightOut .5s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    width: 100%;
+  }
 
-    @keyframes slideRightIn {
-        from {transform: translateX(100%)}
-        to {transform: translateX(0)}
+  @keyframes slideRightIn {
+    from {
+      transform: translateX(100%)
     }
-    @keyframes slideRightOut {
-        from {transform: translateX(0)}
-        to {transform: translateX(-100%)}
+    to {
+      transform: translateX(0)
     }
+  }
 
-     .slide-left-enter-active {
-        animation: slideLeftIn 5s
+  @keyframes slideRightOut {
+    from {
+      transform: translateX(0)
     }
+    to {
+      transform: translateX(-100%)
+    }
+  }
 
-    .slide-left-leave-active {
-        animation: slideLeftOut 5s;
-        position: absolute;
-        top: 0;
-        left: 0;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-    }
+  .slide-left-enter-active {
+    animation: slideLeftIn .5s
+  }
 
-    @keyframes slideLeftIn {
-        from {transform: translateX(-100%)}
-        to {transform: translateX(0)}
+  .slide-left-leave-active {
+    animation: slideLeftOut .5s;
+    position: absolute;
+    top: 0;
+    left: 0;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+  }
+
+  @keyframes slideLeftIn {
+    from {
+      transform: translateX(-100%)
     }
-    @keyframes slideLeftOut {
-        from {transform: translateX(0)}
-        to {transform: translateX(100%)}
+    to {
+      transform: translateX(0)
     }
+  }
+
+  @keyframes slideLeftOut {
+    from {
+      transform: translateX(0)
+    }
+    to {
+      transform: translateX(100%)
+    }
+  }
 </style>
